cae396688bc14b4b16a1aaa906d5cb39080336df
Paulo Victor Nooblath Medeiros <Paulov2001@outlook.com>
2021-04-30 17:33:18 -0300

    exercicio javascript parte2

--- Aula12ex/ex014/script.js
+++ Aula12ex/ex014/script.js
@@ -6,6 +6,15 @@ function carregar(){
     msg.innerHTML = `Agora são ${hora} horas`
     if (hora >= 0 && hora <=12) {
         //BOM DIA!
+        img.src = 'pexels-manha.jpg'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora <= 18){
+        //BOA TARDE!
+        img.src = 'pexels-tarde.jpg'
+        document.body.style.background = '#b9846f'
+    } else{
+        img.src = 'pexels-noite.jpg'
+        document.body.style.background = '#515154'
     }
 }
 
