f5a3e164824d2989a6e1a024e1c1daa6a8a7b280
victornooblath <Paulov2001@outlook.com>
2022-03-16 16:42:27 -0300

    fim_aula13

---
+++ aula13/ambiente.js
@@ -0,0 +1,13 @@
+/*Using "while" to test first and exe after the condition == true */ 
+var c =1 
+while(c <= 6) {
+    console.log(`olá ${c} vezes`)
+    c++
+}
+
+/*Using "Do while" to exe and test*/
+var c = 1
+do {
+    console.log(`olá ${c} vezes`)
+    c++
+} while (c <= 6)
